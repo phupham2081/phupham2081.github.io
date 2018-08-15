7473dbad0712b30b933905cab3f2a00a0b039b27
phupham2081 <40382212+phupham2081@users.noreply.github.com>
2018-08-15 16:21:31 +0700

    update

---
+++ calculator/main.js
@@ -0,0 +1,11 @@
+const calculator = document.querySelector('.app');
+const keys = calculator.querySelector('.keys');
+
+keys.addEventListener('click', e => {
+	if (e.target.matchs('button')) {
+		// do something
+	}
+});
+
+const key = e.target;
+
